ef0402e8ad6f161a7a730f4259d93b3c2cb20f7a
Olena Borysenko <Olena_Borysenko@epam.com>
2016-04-15 17:07:49 +0300

    bem start

---
+++ olena_borysenko/task/sopia/gulpfile.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Olena_Borysenko on 4/14/2016.
+ */
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('default', ['sass', 'sass:watch']);
+
+gulp.task('sass', function (){
+    gulp.src('./sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'))
+
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+});
