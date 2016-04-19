149ed7fbfcdcbf95de32c084f0ee63fdffdcf3f8
Olena Borysenko <Olena_Borysenko@epam.com>
2016-04-19 16:31:42 +0300

    start footer

--- olena_borysenko/task/sopia/gulpfile.js
+++ olena_borysenko/task/sopia/gulpfile.js
@@ -11,7 +11,7 @@ gulp.task('default', ['sass', 'sass:watch']);
 gulp.task('sass', function (){
     gulp.src('./sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css'))
+        .pipe(gulp.dest('./build/css'))
 
 });
 
