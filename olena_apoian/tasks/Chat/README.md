333e30820a5e055632d91e9998eea753b83d705b
Olena Apoian <Olena_Apoian@epam.com>
2016-07-18 11:54:39 +0300

    Added folder structure with webpack.config.js

---
+++ olena_apoian/tasks/Chat/README.md
@@ -0,0 +1,14 @@
+# Demo webpack and React app
+
+This is the entire code repository for the [Getting started with webpack and React, ES6 style](http://humaan.com/getting-started-with-webpack-and-react-es6-style/) article on the Humaan blog.
+
+## In use:
+
+- React
+- webpack
+- Sass
+- Babel
+- ES6 syntax
+- hot module loaders
+
+Feel free to post an issue if you find something amiss.
---
+++ olena_apoian/tasks/Chat/src/js/HelloBox.js
@@ -0,0 +1,17 @@
+// Import React and HelloText class
+import React from 'react';
+import HelloText from './HelloText';
+
+// Create class called HelloBox that extends the base React Component class
+//export default class HelloBox extends React.Component {
+//    constructor() {
+//        super();
+//    }
+//
+//    render() {
+//        return <div>
+//            <HelloText name="Dan" />
+//        </div>
+//    }
+//}
+document.getElementById("logo").classList.add("blue");
