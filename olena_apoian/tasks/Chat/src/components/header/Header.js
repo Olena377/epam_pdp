76577e81de6cda627329d043761658a63d682c80
Olena Apoian <Olena_Apoian@epam.com>
2016-09-30 15:17:24 +0300

    Added onclickoutside

--- olena_apoian/tasks/Chat/src/components/header/Header.js
+++ olena_apoian/tasks/Chat/src/components/header/Header.js
@@ -6,23 +6,17 @@ class Header extends React.Component {
     constructor() {
         super();
     }
-    showMenu() {
-        this.refs.menu.show();
-    };
     render(){
         return (
             <header className="header">
                 <a href="#" className="fa fa-arrow-left fa-lg header__icon--left"></a>
                 <h2 className="header__name">{this.props.friend.name}<span className="header__message">&nbsp; is typing...</span></h2>
-                <a href="#" className="fa fa-ellipsis-h fa-lg header__icon--right" onClick={this.showMenu.bind(this)}></a>
-                <Menu ref="menu"></Menu>
+                <Menu></Menu>
             </header>
 
         )
     }
 };
-
-
 Header.propTypes = {
     name: React.PropTypes.string,
 };
--- olena_apoian/tasks/Chat/src/components/menu/Menu.js
+++ olena_apoian/tasks/Chat/src/components/menu/Menu.js
@@ -13,24 +13,27 @@ export class Menu extends React.Component {
         this.setState({ visible: !this.state.visible});
     };
     handleClickOutside(event) {
-        console.log('Clicked outside.');
+        return this.state.visible ? this.show() : false;
     }
 
     render() {
         return (
-            <ul enableOnClickOutside={true} className={this.state.visible ? "header__nav " : "header__nav display-none"} >
-                <li className="header__item">
-                    <a href="#" className="fa fa-star fa-2x header__anchor" aria-hidden="true"></a>
-                </li>
-                <li className="header__item">
-                    <a href="#" className="fa fa-phone fa-2x header__anchor" aria-hidden="true"></a>
-                </li>
-                <li className="header__item">
-                    <a href="#" className="fa fa-video-camera fa-2x header__anchor" aria-hidden="true"></a>
-                </li>
-            </ul>
+            <div>
+                <a href="#" className="fa fa-ellipsis-h fa-lg header__icon--right" onClick={this.show.bind(this)}></a>
+                <ul className={this.state.visible ? "header__nav " : "header__nav display-none"} >
+                    <li className="header__item">
+                        <a href="#" className="fa fa-star fa-2x header__anchor" aria-hidden="true"></a>
+                    </li>
+                    <li className="header__item">
+                        <a href="#" className="fa fa-phone fa-2x header__anchor" aria-hidden="true"></a>
+                    </li>
+                    <li className="header__item">
+                        <a href="#" className="fa fa-video-camera fa-2x header__anchor" aria-hidden="true"></a>
+                    </li>
+                </ul>
+            </div>
         )
     }
 };
 
-export default OnClickOutside(Menu)
+export default OnClickOutside(Menu);
